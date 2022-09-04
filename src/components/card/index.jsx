bd2b0af1dae5871f1e7635ec48859a06faf5b5f1
Rafael Tivane <luislangabusiness@gmail.com>
2022-09-04 14:56:08 +0200

    add bookmark button

--- src/components/card/index.jsx
+++ src/components/card/index.jsx
@@ -5,19 +5,28 @@ import Image from "next/image";
 import IconHome from "public/icons/icon-nav-home.svg";
 import IconMovies from "public/icons/icon-nav-movies.svg";
 import IconTv from "public/icons/icon-nav-tv-series.svg";
+import IconBookmarkEmpty from "public/icons/icon-bookmark-empty.svg";
+import IconBookmarkFull from "public/icons/icon-bookmark-full.svg";
 
 import classes from "./styles.module.css";
 
-const Card = ({ thumbnail }) => {
+const Card = ({ isBookmarked, title, thumbnail }) => {
 
     return (
-        <li className={classNames(classes.container, `mb-3`)}>
+        <li className={classNames(classes.container, `mb-3 text-white`)}>
             <div className={classNames(classes.imageContainer, `relative`)}>
                 <Image 
                     layout="fill"
                     src={thumbnail.regular.small}
                 />
+                <button className={classNames(classes.button, 
+                    'absolute border-0 flex items-center justify-center outline-none rounded-full')}>
+                    { isBookmarked ? <IconBookmarkFull /> : <IconBookmarkEmpty /> }
+                </button>
             </div>
+            <h3 className="font-bold mt-2">
+                { title }
+            </h3>
         </li>
     );
 };
