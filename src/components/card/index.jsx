2a4ee5461aa88e57bc8bcc8452272a79e3114661
Rafael Tivane <luislangabusiness@gmail.com>
2022-09-05 07:35:42 +0200

    add font family link, card's category, rating and release year

--- src/components/card/index.jsx
+++ src/components/card/index.jsx
@@ -3,17 +3,21 @@ import classNames from "classnames";
 import Image from "next/image";
 
 import IconHome from "public/icons/icon-nav-home.svg";
-import IconMovies from "public/icons/icon-nav-movies.svg";
+import IconMovies from "public/icons/icon-category-movie.svg";
 import IconTv from "public/icons/icon-nav-tv-series.svg";
 import IconBookmarkEmpty from "public/icons/icon-bookmark-empty.svg";
 import IconBookmarkFull from "public/icons/icon-bookmark-full.svg";
 
 import classes from "./styles.module.css";
 
-const Card = ({ isBookmarked, title, thumbnail }) => {
+const Card = ({ category, isBookmarked, rating, title, thumbnail, year }) => {
+    const icons = {
+        Movie: <IconMovies />,
+        "TV Series": <IconTv className="text-white" />
+    };
 
     return (
-        <li className={classNames(classes.container, `mb-3 text-white`)}>
+        <li className={classNames(classes.container, `mb-6 text-white`)}>
             <div className={classNames(classes.imageContainer, `relative`)}>
                 <Image 
                     layout="fill"
@@ -24,7 +28,15 @@ const Card = ({ isBookmarked, title, thumbnail }) => {
                     { isBookmarked ? <IconBookmarkFull /> : <IconBookmarkEmpty /> }
                 </button>
             </div>
-            <h3 className="font-bold mt-2">
+            <ul className="flex items-center mt-3 opacity-90 pl-1 text-xs">
+                <li>{ year }</li>
+                <li className={classNames(classes.categoryItem, "px-4 relative")}>
+                    { icons[category] }
+                    <span className="ml-1">{ category }</span>
+                </li>
+                <li>{ rating }</li>
+            </ul>
+            <h3 className="font-semibold mt-2 pl-1 text-base">
                 { title }
             </h3>
         </li>
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,6 +2,7 @@ import * as React from 'react';
 import Head from "next/head";
 
 import "src/styles/reset.css";
+import "src/styles/global.css"
 import 'src/styles/tailwind.css';
 
 import Layout from "src/components/layout"
@@ -12,6 +13,10 @@ const MyApp = ({ Component, pageProps }) => {
         <>
             <Head>
                 <meta name="viewport" content="initial-scale=1, width=device-width" />
+                <link
+                    rel="stylesheet"
+                    href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,900&amp;display=swap"
+                />
             </Head>
             <Layout>
                 <Component {...pageProps} />
