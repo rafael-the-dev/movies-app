7055116aa0f1ffa8f5e04466fafa6bffe5ae085b
Rafael Tivane <luislangabusiness@gmail.com>
2022-09-05 10:53:36 +0200

    slide carousel

---
+++ src/components/carousel-controllers/index.jsx
@@ -0,0 +1,76 @@
+import { useCallback, useEffect, useMemo, useState } from "react";
+
+import classNames from "classnames";
+import ArrowIcon from "public/icons/icon-arrow.svg"
+
+const CarouselControllers = ({ indexRef, slide, setChildrenListRef }) => {
+    const [ index, setIndex ] = useState(0);
+    const [ childrenList, setChildrenList ] = useState([]); 
+
+    const hasPreviousItem = useMemo(() => index - 1 < 0, [ index ]);
+    const hasNextItem = useMemo(() => index + 1 >= childrenList.length, [ childrenList, index ]);
+
+    const nextItemClickHandler = useCallback(() => {
+        setIndex(currentIndex => {
+            return currentIndex + 1;
+        })
+    }, []);
+
+    const previousItemClickHandler = useCallback(() => {
+        setIndex(currentIndex => {
+            return currentIndex - 1;
+        })
+    }, []);
+
+    useEffect(() => {
+        setChildrenListRef.current = list => setChildrenList(list);
+    }, [ setChildrenListRef ]);
+
+    useEffect(() => {
+        indexRef.current = index;
+        slide({ index });
+    }, [ index, slide ]);
+
+    return (
+        <div className="absolute controllers-container flex justify-between w-full">
+            <button 
+                className={classNames(`controllers__button controllers__button--previous flex items-center 
+                jusitfy-center rounded-full`, { "opacity-0": hasPreviousItem})}
+                disabled={hasPreviousItem}
+                onClick={previousItemClickHandler}>
+                <ArrowIcon />
+            </button>
+            <button 
+                className={classNames(`controllers__button flex items-center jusitfy-center rounded-full`,
+                { "opacity-0": hasNextItem})}
+                disabled={hasNextItem}
+                onClick={nextItemClickHandler}>
+                <ArrowIcon />
+            </button>
+            <style jsx>
+                {
+                    `
+                        .controllers-container {
+                            left: 0;
+                            top: 50%;
+                            transform: translate(0, -50%);
+                            width: 95%;
+                        }
+
+                        .controllers__button {
+                            background-color: #97979794;
+                            height: 40px;
+                            width: 40px;
+                        }
+
+                        .controllers__button--previous {
+                            transform: rotate(180deg)
+                        }
+                    `
+                }
+            </style>
+        </div>
+    );
+};
+
+export default CarouselControllers;
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,12 +3,14 @@ import data from "public/data.json";
 
 import Card from "src/components/card";
 import TrendingCard from "src/components/trending-card";
+import CarouselControllers from "src/components/carousel-controllers";
 
 const Home = () => {
     const list = data.filter(item => !item.isTrending);
     const childrenList = useRef([]);
     const currentIndex = useRef(0);
     const sliderRef = useRef(null);
+    const setChildrenListRef = useRef(null);
 
     const layout = useCallback(() => {
         const { innerWidth } = window;
@@ -23,18 +25,26 @@ const Home = () => {
                 child.style.left = `${(innerWidth - 70) * index}px`;
             }
         })
+    }, []);
+
+    const slide = useCallback(({ index }) => {
+        const { innerWidth } = window;
+        let width = innerWidth - 70;
+        const slideIndex = index ?? currentIndex.current;
+        sliderRef.current.style.transform = `translateX(-${slideIndex * width}px)`
     }, [])
 
     useEffect(() => {
         const list = [ ...sliderRef.current.children ];
         childrenList.current = list;
         layout();
+        setChildrenListRef.current?.(list);
     }, [ layout ])
 
     return (
         <>
-            <main className="px-4 pt-4">
-                <section className="overflow-hidden w-100">
+            <main className="pt-4">
+                <section className="overflow-hidden pl-4 relative w-full">
                     <ul 
                         className="flex items-stretch justify-between relative trending-list"
                         ref={sliderRef}>
@@ -44,8 +54,13 @@ const Home = () => {
 
                         }
                     </ul>
+                    <CarouselControllers 
+                        indexRef={currentIndex}
+                        slide={slide} 
+                        setChildrenListRef={setChildrenListRef} 
+                    />
                 </section>
-                <section className="mt-8">
+                <section className="mt-8 px-4">
                     <ul className="flex flex-wrap items-stretch justify-between">
                         {
                             list.map((item, index) => <Card { ...item } key={index} />)
@@ -57,7 +72,7 @@ const Home = () => {
                         `
                             .trending-list {
                                 height: 180px;
-                                transition: transform 1s ease;
+                                transition: transform 1.5s ease;
                             }
                         `
                     }
