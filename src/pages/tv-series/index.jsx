3b3fd2d144efd6faac3f2d2779fb0a585b24297b
Rafael Tivane <luislangabusiness@gmail.com>
2022-09-22 08:42:00 +0200

    add search form to tv series pages

--- src/pages/tv-series/index.jsx
+++ src/pages/tv-series/index.jsx
@@ -1,8 +1,11 @@
+import { useMemo, useState } from "react";
 
 import data from "public/data.json";
 
 import Card from "src/components/card";
+import Form from "src/components/search-form";
 import List from "src/components/list";
+import Main from "src/components/main";
 import Title from "src/components/title";
 
 export async function getStaticProps() {
@@ -16,18 +19,43 @@ export async function getStaticProps() {
 };
 
 const Container = ({ tvSeriesList }) => {
+    const [ searchList, setSearchList ] = useState([]);
+
+    const formMemo = useMemo(() => (
+        <div className="px-4">
+            <Form 
+                data={[ ...tvSeriesList ]}
+                setData={setSearchList}
+            />
+        </div>
+    ), []);
+
+    const tvSeriesSection = useMemo(() => (
+        <section className="mt-8 px-4 xl:pr-4">
+            <Title>TV Series</Title>
+            <List>
+                {
+                    tvSeriesList.map((item, index) => <Card { ...item } key={index} />)
+                }
+            </List>
+        </section>
+    ), [ tvSeriesList ])
 
     return (
-        <main className="main overflow-y-auto pt-4 xl:pl-4">
-            <section className="mt-8 px-4 xl:pr-4">
-                <Title>TV Series</Title>
-                <List>
-                    {
-                        tvSeriesList.map((item, index) => <Card { ...item } key={index} />)
-                    }
-                </List>
-            </section>
-        </main>
+        <Main>
+            { formMemo }
+            {
+                searchList.length > 0 ? (
+                    <div className="mt-8 px-4 xl:pr-4">
+                        <List>
+                            {
+                                searchList.map((item, index) => <Card { ...item } key={`${item.title}${index}`} />)
+                            }
+                        </List>
+                    </div>
+                ) : tvSeriesSection
+            }
+        </Main>
     );
 };
 
